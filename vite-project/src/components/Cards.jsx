a84df84901035b6806a603a5a25d5ecfa80511d4
Pragyan Khaniya <pragyanfrontend@gmail.com>
2023-08-11 23:08:38 +0545

    feat:added delete

--- vite-project/src/components/Cards.jsx
+++ vite-project/src/components/Cards.jsx
@@ -3,7 +3,8 @@ import Card from 'react-bootstrap/Card';
 
 
 
-function CardDisplay({data}){
+function CardDisplay({data,deletehandler}){
+
 
 return(
     <Card  style={{ width: '18rem' }}>
@@ -13,7 +14,19 @@ return(
         <Card.Text>
          {data.description}
         </Card.Text>
-        <Button variant="danger">Delete</Button>
+        <Card.Text>
+         Price: Rs{data.price}
+        </Card.Text>
+        <Card.Text>
+        Rate: {data.rating}
+        </Card.Text>
+        <Card.Text>
+        Brand: {data.brand}
+        </Card.Text>
+         
+           
+
+        <Button onClick={(e)=>deletehandler(data.id)} variant="danger">Delete</Button>
       </Card.Body>
     </Card>
 
